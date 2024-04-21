31a4d6feed1a4aa7ae95c1a14ebbae94b98221cb
Krupa <krupa.john@bds.christuniversity.in>
2024-04-21 12:34:37 +0530

    commit

--- interest_calculator/urls.py
+++ interest_calculator/urls.py
@@ -14,16 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""from django.contrib import admin
-from django.urls import path,include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('calculator.urls')),   # This is to include the apps urls in this
-]
-"""
-
-# urls.py
 from django.contrib import admin
 from django.urls import path,include
 from calculator.views import index, delete_calculation  # Import the delete_calculation view
